fcfb2f6d4b4327de2960099e35613a5ce5be58f1
Chun-gu <chungu.yi92@gmail.com>
2023-10-22 11:50:55 +0900

    feat: add a function that generates random string of number

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,7 @@ import type { ClassValue } from 'clsx'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateRandomStringOfNumber(digits: number) {
+  return Math.floor(Math.random() * Math.pow(10, digits)).toString()
+}
