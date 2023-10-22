b0744947bc3e3989051448318054785d685808cd
Chun-gu <chungu.yi92@gmail.com>
2023-10-22 09:24:12 +0900

    feat: display names & questions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 
 export default function Home() {
   const [studyDates, setStudyDates] = useState<Array<StudyDate>>([])
+  const [names, setNames] = useState([])
+  const [questions, setQuestions] = useState([])
 
   useEffect(() => {
     const abortController = new AbortController()
@@ -29,6 +31,23 @@ export default function Home() {
     }
   }, [])
 
+  async function onChange(e: ChangeEvent<HTMLSelectElement>) {
+    const dateId = e.currentTarget.value
+
+    try {
+      const res = await fetch(`/api/study-dates/${dateId}/names-questions`, {
+        method: 'GET',
+      })
+      if (!res.ok) throw Error
+
+      const { names, questions } = await res.json()
+      setNames(names)
+      setQuestions(questions)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <>
       <header className="border-b py-4 text-center text-2xl font-extrabold">
@@ -43,6 +62,7 @@ export default function Home() {
           <select
             name="studyDates"
             id="studyDates"
+            onChange={onChange}
             className="rounded-md bg-gray-200 p-1">
             {studyDates.map((studyDate) => (
               <option key={studyDate.id} value={studyDate.id}>
@@ -51,6 +71,31 @@ export default function Home() {
             ))}
           </select>
         </section>
+        <section className="mb-2">
+          <h2 className="text-lg font-bold">면접 참여자</h2>
+          <ul>
+            {names.map((name) => (
+              <li key={name} className="rounded-md bg-yellow-200 px-2 py-1">
+                <input
+                  type="text"
+                  defaultValue={name}
+                  className="bg-yellow-200"
+                />
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section className="mb-2">
+          <h2 className="text-lg font-bold">질문 목록</h2>
+          <ul>
+            {questions.map((question) => (
+              <li key={question}>
+                <p>{question}</p>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <button>시작</button>
       </main>
     </>
   )
