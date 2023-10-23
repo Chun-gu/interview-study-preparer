7577abdc66c6ab5b136d4ef77c66aaa4c44d66dc
Chun-gu <chungu.yi92@gmail.com>
2023-10-23 16:51:27 +0900

    feat: create slice for `questions`

---
+++ src/store/slices/questionSlice.ts
@@ -0,0 +1,33 @@
+import { StateCreator } from 'zustand'
+
+export interface QuestionSlice {
+  questions: Array<Question>
+  setQuestions: (questions: Array<Question>) => void
+  createQuestion: (question: Question) => void
+  updateQuestion: (question: Question) => void
+  deleteQuestion: (targetId: Question['id']) => void
+}
+
+export const createQuestionSlice: StateCreator<
+  QuestionSlice,
+  [],
+  [],
+  QuestionSlice
+> = (set) => ({
+  questions: [],
+  setQuestions: (questions) => set(() => ({ questions })),
+  createQuestion: (question) =>
+    set((prev) => ({ questions: [...prev.questions, question] })),
+  updateQuestion: (question) =>
+    set((prev) => ({
+      questions: prev.questions.map((prevQuestion) =>
+        prevQuestion.id === question.id ? question : prevQuestion,
+      ),
+    })),
+  deleteQuestion: (targetId) =>
+    set((prev) => ({
+      questions: prev.questions.filter(
+        (prevQuestion) => prevQuestion.id !== targetId,
+      ),
+    })),
+})
