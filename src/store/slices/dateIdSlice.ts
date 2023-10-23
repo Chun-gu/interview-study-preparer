fdb7fb3ef0fc559e8f9d9388b22dd793f7a3013a
Chun-gu <chungu.yi92@gmail.com>
2023-10-23 16:50:35 +0900

    feat: create slice for `dateId`

---
+++ src/store/slices/dateIdSlice.ts
@@ -0,0 +1,16 @@
+import { StateCreator } from 'zustand'
+
+export interface DateIdSlice {
+  dateId: string
+  setDateId: (dateId: string) => void
+}
+
+export const createDateIdSlice: StateCreator<
+  DateIdSlice,
+  [],
+  [],
+  DateIdSlice
+> = (set) => ({
+  dateId: '',
+  setDateId: (dateId) => set(() => ({ dateId })),
+})
