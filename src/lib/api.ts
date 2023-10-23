6be5bc336650075c5b993b2f63ae6fe208c7953a
Chun-gu <chungu.yi92@gmail.com>
2023-10-23 17:04:11 +0900

    feat: add fetching functions for `studydates`, `participants`, `questions`

---
+++ src/lib/api.ts
@@ -0,0 +1,51 @@
+export async function getStudyDates(): Promise<Array<StudyDate>> {
+  try {
+    const res = await fetch('/api/study-dates', {
+      method: 'GET',
+    })
+
+    if (!res.ok) throw new Error(`${res.status}`)
+
+    const { studyDates } = await res.json()
+
+    return studyDates
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message)
+    else throw new Error('unknown error')
+  }
+}
+
+export async function getParticipants(
+  dateId: string,
+): Promise<Array<Participant>> {
+  try {
+    const res = await fetch(`/api/study-dates/${dateId}/participants`, {
+      method: 'GET',
+    })
+
+    if (!res.ok) throw new Error(`${res.status}`)
+
+    const { participants } = await res.json()
+
+    return participants
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message)
+    else throw new Error('unknown error')
+  }
+}
+
+export async function getQuestions(dateId: string): Promise<Array<Question>> {
+  try {
+    const res = await fetch(`/api/study-dates/${dateId}/questions`, {
+      method: 'GET',
+    })
+    if (!res.ok) throw new Error(`${res.status}`)
+
+    const { questions } = await res.json()
+
+    return questions
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message)
+    else throw new Error('unknown error')
+  }
+}
