4dc0b548e4e3ed864bbc612aff21e3f002c61276
Chun-gu <chungu.yi92@gmail.com>
2023-10-23 16:51:01 +0900

    feat: create slice for `participants`

---
+++ src/store/slices/participantSlice.ts
@@ -0,0 +1,33 @@
+import { StateCreator } from 'zustand'
+
+export interface ParticipantSlice {
+  participants: Array<Participant>
+  setParticipants: (participants: Array<Participant>) => void
+  createParticipant: (participant: Participant) => void
+  updateParticipant: (participant: Participant) => void
+  deleteParticipant: (targetId: Participant['id']) => void
+}
+
+export const createParticipantSlice: StateCreator<
+  ParticipantSlice,
+  [],
+  [],
+  ParticipantSlice
+> = (set) => ({
+  participants: [],
+  setParticipants: (participants) => set(() => ({ participants })),
+  createParticipant: (participant) =>
+    set((prev) => ({ participants: [...prev.participants, participant] })),
+  updateParticipant: (participant) =>
+    set((prev) => ({
+      participants: prev.participants.map((prevParticipant) =>
+        prevParticipant.id === participant.id ? participant : prevParticipant,
+      ),
+    })),
+  deleteParticipant: (targetId) =>
+    set((prev) => ({
+      participants: prev.participants.filter(
+        (prevParticipant) => prevParticipant.id !== targetId,
+      ),
+    })),
+})
