59ee08bc45fe8927bf1116bbd4916d349c34e21f
Chun-gu <chungu.yi92@gmail.com>
2023-10-24 22:35:47 +0900

    feat: add refresh button

---
+++ src/components/RefreshButton.tsx
@@ -0,0 +1,27 @@
+import { ComponentProps } from 'react'
+
+import RefreshIcon from '@/assets/icons/refresh.svg'
+import { cn } from '@/lib/utils'
+
+type Props = ComponentProps<'button'> & { isRefetching?: boolean }
+
+export default function RefreshButton({
+  children,
+  className,
+  onClick,
+  isRefetching,
+}: Props) {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        'flex aspect-square h-full items-center justify-center rounded-md bg-gray-100 hover:bg-gray-200',
+        className,
+      )}>
+      <span className="sr-only">{children}</span>
+      <RefreshIcon
+        className={`h-5 w-5 ${isRefetching && 'hover:animate-spin'}`}
+      />
+    </button>
+  )
+}
