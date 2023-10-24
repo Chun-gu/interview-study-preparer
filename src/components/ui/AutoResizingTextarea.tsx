ec87a8a23091b91248437fe658d90687a1aab549
Chun-gu <chungu.yi92@gmail.com>
2023-10-24 22:32:34 +0900

    feat: add `AutoResizingTextarea`

---
+++ src/components/ui/AutoResizingTextarea.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import {
+  ComponentPropsWithoutRef,
+  forwardRef,
+  useLayoutEffect,
+  useRef,
+} from 'react'
+
+import { cn } from '@/lib/utils'
+
+const AutoResizingTextarea = forwardRef<
+  HTMLTextAreaElement,
+  ComponentPropsWithoutRef<'textarea'>
+>(({ className, ...props }, forwardedRef) => {
+  const innerRef = useRef<HTMLTextAreaElement>(null)
+
+  function resizeTextarea() {
+    if (!innerRef.current) return
+
+    innerRef.current.style.height = 'auto'
+
+    const scrollHeight = innerRef.current.scrollHeight
+    const { borderTopWidth, borderBottomWidth } = window.getComputedStyle(
+      innerRef.current,
+    )
+    const borderWidth =
+      parseFloat(borderTopWidth) + parseFloat(borderBottomWidth)
+
+    innerRef.current.style.height = `${scrollHeight + borderWidth}px`
+  }
+
+  function composeRef(el: HTMLTextAreaElement) {
+    if (innerRef.current) return
+
+    if (typeof forwardedRef === 'function') forwardedRef(el)
+    else if (forwardedRef) forwardedRef.current = el
+    ;(innerRef as Writable<typeof innerRef>).current = el
+  }
+
+  function onChange() {
+    resizeTextarea()
+  }
+
+  useLayoutEffect(() => {
+    resizeTextarea()
+  })
+
+  return (
+    <textarea
+      ref={composeRef}
+      rows={1}
+      onChange={onChange}
+      className={cn('resize-none', className)}
+      {...props}
+    />
+  )
+})
+
+AutoResizingTextarea.displayName = 'AutoResizingTextarea'
+export default AutoResizingTextarea
