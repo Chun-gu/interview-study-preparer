8d585a932a3542bd3432bbef2a29a02b1f438f87
Chun-gu <chungu.yi92@gmail.com>
2023-10-24 22:37:00 +0900

    feat: add fallback component for error state

---
+++ src/components/ErrorFallback.tsx
@@ -0,0 +1,29 @@
+import { QueryObserverResult, RefetchOptions } from '@tanstack/react-query'
+
+import { cn } from '@/lib/utils'
+
+type Props = {
+  className?: string
+  refetch?: (
+    options?: RefetchOptions | undefined,
+  ) => Promise<QueryObserverResult<unknown, Error>>
+}
+
+export default function ErrorFallback({ className, refetch }: Props) {
+  return (
+    <div
+      className={cn(
+        'flex h-[68px] w-full flex-col items-center justify-center rounded-md border border-red-500 bg-gray-100',
+        className,
+      )}>
+      <p className="font-bold text-red-500">에러가 발생했어요!</p>
+      {!!refetch && (
+        <button
+          onClick={() => refetch()}
+          className="rounded-md bg-red-500 px-3 py-1 text-sm text-white">
+          재시도
+        </button>
+      )}
+    </div>
+  )
+}
