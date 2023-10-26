9efe2e9e26eec1777798572af04d7dda40896870
Chun-gu <chungu.yi92@gmail.com>
2023-10-26 18:51:52 +0900

    type: add type for result

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 type StudyDate = { id: string; date: string }
 type Participant = { id: string; name: string }
 type Question = { id: string; content: string }
+type Result = { participant: Participant; questions: Array<Question> }
 
 type Writable<T> = { -readonly [K in keyof T]: T[K] }
