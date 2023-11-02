c6ef7b4c5bbe1c0f9d0b9e99a94b2c6900c09c44
Chun-gu <chungu.yi92@gmail.com>
2023-11-03 01:00:11 +0900

    fix: add route segment config 'force-dynamic` to opt out cache

--- src/app/api/study-dates/route.ts
+++ src/app/api/study-dates/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 
 import { serverSideFetch } from '../_lib/serverSideFetch'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request) {
   try {
     const res = await serverSideFetch(
